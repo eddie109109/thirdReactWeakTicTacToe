0905e5923e17442cdadf76c525cc56f58630ec63
jundic <jundi_chen@sfu.ca>
2020-08-27 12:40:38 -0700

    Updates

---
+++ precache-manifest.69c7fd411b372ab9429165ac8652d308.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "709c45697b2df6d7283122022b75258c",
+    "url": "/thirdReactWeakTicTacToe/index.html"
+  },
+  {
+    "revision": "80d3450165f843861cb4",
+    "url": "/thirdReactWeakTicTacToe/static/js/2.ad600bbe.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/thirdReactWeakTicTacToe/static/js/2.ad600bbe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f834ea4728249257eac",
+    "url": "/thirdReactWeakTicTacToe/static/js/main.7a3b5bee.chunk.js"
+  },
+  {
+    "revision": "a6e1fd4f8e72ce765bde",
+    "url": "/thirdReactWeakTicTacToe/static/js/runtime-main.e9a1e0ef.js"
+  }
+]);
